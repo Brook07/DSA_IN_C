1db475e457be2fb82eda9c682cfef66f6f83712c
Brook07 <utsavadhr@gmail.com>
2024-11-29 21:35:46 +0545

    Added Comments to the code

--- stack_implementation.c
+++ stack_implementation.c
@@ -1,28 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//defining the max size of the arrary 
 #define MAX 10
 
 typedef struct Stack{
-    int items[MAX];
-    int top;
+    int items[MAX];         //Array to hold the stack elements 
+    int top;                //Index of the top element in the stack
 }Stack;
 
+//initialiazating the top 
 void initialize(Stack *s){
-    s->top = -1;
+    s->top = -1; //Sets the top to -1 indicating the stack is empty
 }
 
+//check if the stack is full 
+//Returns 1 (true) if the stack is full (top reaches the maximum index MAX - 1), otherwise 0.
 int isFull(Stack* s){
     return s->top == MAX - 1;
 }
+//
+
+//check if the stack is empty 
 int isEmpty(Stack *s){
     return s->top == -1;
 }
+
+
  void push(Stack* s,int value){
     if (isFull(s)){
         printf("Stack Overflow!!\n");
     }else {
-        s->items[++(s->top)] = value ;
+        s->items[++(s->top)] = value ;      //Increment the top and adds the value to the top
         printf("pushed %d onto the stack.\n", value);
     }
  }
