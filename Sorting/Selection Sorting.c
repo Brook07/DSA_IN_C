caf4dd9f29fcf8414520d16b44bf6f99fd65cd0b
Utsav Adhikari <158271037+Brook07@users.noreply.github.com>
2025-01-25 17:55:22 +0545

    selection Sorting program added

---
+++ Sorting/Selection Sorting.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int i,j,temp,minIndex;
+void SelectionSort(int arr[],int n){
+    for (i=0;i<n-1;i++){
+        minIndex = i;
+        for(j=i+1;j<n;j++){
+        if(arr[j]<arr[minIndex])
+        {
+            minIndex = j;
+        }
+    }
+     if (minIndex != i){
+            // Swap the elements
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+void printArray(int arr[],int n){
+    for(i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+}
+
+int main(){
+    int arr[]={6,2,8,4,5};
+    int n=5;
+    SelectionSort(arr,n);
+    printf("Sorted Array :\n");
+    printArray(arr,n);
+    return 0;
+}
