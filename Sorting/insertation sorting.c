777b3a7198c80e5bbe5749c66deebcea75405650
Utsav Adhikari <158271037+Brook07@users.noreply.github.com>
2025-01-26 20:04:10 +0545

    program reworked

--- Sorting/insertation sorting.c
+++ Sorting/insertation sorting.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 
-void insertion_sorting(int arr[],int n){
-    int minIndex,temp,i,j;
-    for(i=0;i<n-1;i++){
-        minIndex = i;
-    for(j=i+1;j<n;j++){
-        if(arr[minIndex]>arr[j]){
-            minIndex = j;
-            }
+void insertion_sorting(int arr[], int n) {
+    int i, j, key;
+    
+    for (i = 1; i < n; i++) {
+        key = arr[i];  // The element to be inserted
+        j = i - 1;
+
+        // Shift elements of the sorted portion that are greater than the key
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        if (minIndex != i){
-            temp=arr[minIndex];
-            arr[minIndex]=arr[j];
-            arr[j]=temp;
-        }
+
+        // Place the key in its correct position
+        arr[j + 1] = key;
     }
 }
 void print_array(int arr[],int n){
@@ -26,7 +27,7 @@ void print_array(int arr[],int n){
 
 //Main function
 int main(){
-    int arr[] = {5,4,3,2,1};
+    int arr[] = {65,4,13,21,12};
     int n=5;
     printf("Unsorted Array : \n");
     print_array(arr,n);
